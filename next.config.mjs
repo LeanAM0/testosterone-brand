e45c9f7820d44b3431867f7dd9d792a1bdec83d0
LeanAM0 <leandro.a.marangoni@gmail.com>
2025-05-31 19:23:49 -0300

    Actualizar next.config.mjs para resolver problemas de despliegue en Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,21 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
+    domains: ['www.notion.so', 'prod-files-secure.s3.us-west-2.amazonaws.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  experimental: {
+    // Esta configuración ayuda con problemas de dependencias
+    esmExternals: 'loose',
+  },
+  // Permitimos imágenes de Notion
+  webpack(config) {
+    return config;
   },
 }
 
