f7a5773bd7aebc8dfbcd2ed0a9df6b2308b81719
LeanAM0 <leandro.a.marangoni@gmail.com>
2025-05-31 18:40:17 -0300

    Añadir utilidades para manejo de clases CSS

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,10 @@
-import { clsx, type ClassValue } from "clsx"
+import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+/**
+ * Combina múltiples clases CSS usando clsx y las optimiza con tailwind-merge
+ * Esto permite combinar clases condicionales y eliminar conflictos entre clases de Tailwind
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
